e622e270a3f80cee049e5a1a3c9ab0c7a3a49d09
DeltaRobotics-FTC <deltaroboticsftc@gmail.com>
2024-03-07 19:31:21 -0600

    Added the emptyOpmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/emptyOpmode.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+@TeleOp(name="emptyOpmode")
+@Disabled
+
+public class emptyOpmode extends LinearOpMode
+{
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+
+        waitForStart();
+
+        while (opModeIsActive())
+        {
+
+        }
+    }
+}
